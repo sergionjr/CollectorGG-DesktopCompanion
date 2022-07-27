51f7976b5f5115be2531701f155e482701724b0d
sergionjr <seno7509@colorado.edu>
2022-07-26 21:02:12 -0600

    Main skeleton finished, picture still not working

--- CollectorGG-DesktopCompanion/Core/RelayCommand.cs
+++ CollectorGG-DesktopCompanion/Core/RelayCommand.cs
@@ -22,7 +22,7 @@ namespace CollectorGG_DesktopCompanion.Core
 
         public bool CanExecute(object parameter)
         {
-            return _canExecute == null | _canExecute(parameter);
+            return _canExecute == null || _canExecute(parameter);
         }
 
         public void Execute(object parameter)
--- CollectorGG-DesktopCompanion/MVVM/ViewModel/MainViewModel.cs
+++ CollectorGG-DesktopCompanion/MVVM/ViewModel/MainViewModel.cs
@@ -9,7 +9,16 @@ namespace CollectorGG_DesktopCompanion.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
+
         public HomeViewModel HomeVM { get; set; }
+
+        public DiscoveryViewModel DiscoveryVM { get; set; }
 
         private object _currentView;
 
@@ -27,7 +36,19 @@ namespace CollectorGG_DesktopCompanion.MVVM.ViewModel
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
             CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
 
         }
     }
